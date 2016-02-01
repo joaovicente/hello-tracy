c6c0b0c271edf7332ae9d441052a63b14d3587fa
Joao Vicente <vicentejo@dnb.com>
2016-02-01 00:06:28 +0000

    Updating run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -jar ./target/hello-tracy-0.0.1-SNAPSHOT.jar | tee /Users/vicentejo/src/Tracy/tracy-demo/tracy-logs/tracy.log
+java -jar ./target/hello-tracy-0.0.1.jar | tee /Users/vicentejo/src/Tracy/tracy-demo/tracy-logs/tracy.log
